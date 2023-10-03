29470a20f9f96d1711af11613dd172faeed15084
troshov <128801794+troshov@users.noreply.github.com>
2023-10-03 23:01:43 +0500

    Update task12.py

--- buns/mod2/task12.py
+++ buns/mod2/task12.py
@@ -4,7 +4,7 @@ while 1:
     if len(a) != 0:
         s = a[0]
         a = a[1::]
-        if s != "-" and s != ")" and s !="(":
+        if s != "-" and s != ")" and s !="(" and s !=" ":
             otv += s
     else:break
 print(otv)
