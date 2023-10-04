11d963ac2041e802f6512ab55118ca94904df529
troshov <128801794+troshov@users.noreply.github.com>
2023-10-04 21:21:03 +0500

    Update task6.py

--- buns/mod2/task6.py
+++ buns/mod2/task6.py
@@ -2,15 +2,15 @@ a = input()
 otv = ''
 while 1:
     if len(a) > 0:
-        st = a[0]
+        st = a[len(a) - 1]
         if st!= '.':
             otv += st
-            a = a[1::]
+            a = a[:-1]            
         else:
-            print(otv)
-            a = a[1::]
+            print(otv[::-1])
+            a = a[:-1]
             otv = ''
     else:
-        print(otv)
+        print(otv[::-1])
         break
         
